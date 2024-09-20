bf0eaa427ae3134470220f2e646d3f09c8a683fb
itsJoel59 <joel.j.jansson@gmail.com>
2024-09-20 07:19:28 +0200

    added function for counting

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ const list = [];
 let count = 0;
 const input = document.querySelector("#userInput");
 const addBtn = document.querySelector("#addBtn");
-const countLbl = document.querySelector();
+const countLbl = document.querySelector("#countList");
+const listHtml = document.querySelector("#todolist");
+
+function countUp(){
+    count++;
+    countLbl.innerHTML = "Count" + count;
+}
